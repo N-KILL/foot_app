e6bd70efbdc28c5a7c302b9a0d5b428b5825527c
Ignacio Cardozo <69329781+N-KILL@users.noreply.github.com>
2025-04-30 08:45:23 -0300

    Update app_test.dart

--- test/app/view/app_test.dart
+++ test/app/view/app_test.dart
@@ -1,2 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
 void main() {
+  testWidgets(
+    'smoke test',
+    (WidgetTester tester) async {
+      await tester.pumpWidget(
+        const MaterialApp(
+          home: Scaffold(
+            body: Text(
+              'Hola mundo',
+            ),
+          ),
+        ),
+      );
+
+      expect(
+        find.text(
+          'Hola mundo',
+        ),
+        findsOneWidget,
+      );
+    },
+  );
 }
